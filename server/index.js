de4030e2af0cb97553065a7dccb3024016edb8d7
Fang Wen Jie <fangwj9566@yahoo.com.sg>
2021-11-11 12:29:12 +0800

    setup connection to mysql

--- server/index.js
+++ server/index.js
@@ -1,11 +1,50 @@
-const express= require('express');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+// const {sequelize} = require('./models')
+const config = require('./src/config/config')
 
 const app = express();
+app.use(morgan('combined'));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
 
-const PORT = 5000;
+// require('./passport')
 
-app.listen(PORT, () => console.log(`SERVER LISTENING ON POST ${PORT}`));
+// require('./routes')(app)
+// app.listen(config.port, () => {
+//   console.log(`Server stated on port ${config.port}`)
+// }) 
 
-app.get('/', (req,res) => {
-  res.send("hello");
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'project_expenses'
 })
+
+connection.connect()
+
+connection.query('SELECT * from category', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('The solution is: ', rows)
+})
+
+connection.end()
+
+
+
+
+
+// sequelize.sync({force:true}).then(() => {
+//   app.listen(config.port)
+//   console.log(`Server started on port ${config.port}`)
+// })
