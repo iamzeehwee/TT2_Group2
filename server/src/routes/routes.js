a385517bb554e6963ce78f978ba26b402faf5ea8
Fang Wen Jie <fangwj9566@yahoo.com.sg>
2021-11-11 14:25:12 +0800

    refactor routes. Added controllers for the individual api

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,19 +1,23 @@
 const AuthenticationController = require("../controllers/AuthenticationController");
-// const AuthenticationControllerPolicy = require("./policies/AuthenticationControllerPolicy");
+const ExpensesController = require("../controllers/ExpenseController");
+const CategoryController = require("../controllers/CategoryController");
+const ProjectController = require("../controllers/ProjectController");
 
 module.exports = (app) => {
-  app.post(
-    "/login",
-    AuthenticationController.login
-  );
+  app.post("/login", AuthenticationController.login);
 
-  app.post(
-    "/register",
-    AuthenticationController.register
-  );
+  app.post("/register", AuthenticationController.register);
+
+  app.post("/expenses", AuthenticationController.register);
+
+  app.post("/expense/add", ExpensesController.addExpenses);
+
+  app.get("/catergory", CategoryController.getCategory);
+
+  app.get("/project/:id", ProjectController.getCategory);
 
   app.get("/:whatYouwantToPut/", (req, res) => {
-    console.log("hohooh", req.body, req.params, req.query)
+    console.log("hohooh", req.body, req.params, req.query);
     res.send("Hello World!");
   });
 };
